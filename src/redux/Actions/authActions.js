32a928263f46855adfe4bbb396b479f7f375089c
madhuy1260 <y.madhu1260@gmail.com>
2025-09-16 16:13:42 +0530

    redux updateed

--- src/redux/Actions/authActions.js
+++ src/redux/Actions/authActions.js
@@ -9,72 +9,7 @@ export const loginAction = (type, data, message, loading) => {
   };
 };
 
-export const registerTenantAction = (type, data, message, loading) => {
-  return {
-    type: type,
-    payload: {
-      data,
-      message,
-      loading,
-    },
-  };
-};
-
 export const registerUserAction = (type, data, message, loading) => {
-  return {
-    type: type,
-    payload: {
-      data,
-      message,
-      loading,
-    },
-  };
-};
-
-export const loginTenantAction = (type, data, message, loading) => {
-  return {
-    type: type,
-    payload: {
-      data,
-      message,
-      loading,
-    },
-  };
-};
-
-export const validateTokenAction = (type, data, message, loading) => {
-  return {
-    type: type,
-    payload: {
-      data,
-      message,
-      loading,
-    },
-  };
-};
-
-export const getUsersByTenantAction = (type, data, message, loading) => {
-  return {
-    type: type,
-    payload: {
-      data,
-      message,
-      loading,
-    },
-  };
-};
-export const getProfileByUserIDAction = (type, data, message, loading) => {
-  return {
-    type: type,
-    payload: {
-      data,
-      message,
-      loading,
-    },
-  };
-};
-
-export const getProfileDetailsAction = (type, data, message, loading) => {
   return {
     type: type,
     payload: {
